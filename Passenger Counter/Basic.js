938ec2c61839d30ee6efa6574c8225f8765a4944
arun0404 <0404arun@gmail.com>
2023-08-14 16:13:08 +0530

    Created Passenger Counter Files

---
+++ Passenger Counter/Basic.js
@@ -0,0 +1,54 @@
+//1. operations
+let count = 50
+count = count + 50
+console.log(count)
+
+count = count - 75
+console.log(count)
+
+count = count + 45
+console.log(count)
+
+//2. function
+function countdown(){
+    console.log(42)
+    console.log(43)
+    console.log(44)
+    console.log(45)
+    console.log(46)
+}
+
+countdown()
+
+//4. practice
+// Create a function that logs out the sum of all the lap times
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function raceTime(){
+    let count = lap1 + lap2 + lap3
+    console.log(count)
+}
+raceTime()
+// Another method
+console.log(lap1+lap2+lap3)
+
+//5. Write a function that increments
+
+let lapsCompleted = 0
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+function increment(){
+    lapsCompleted = lapsCompleted + 1
+    console.log(lapsCompleted)
+}
+
+increment()
+increment()
+increment()
+
+
---
+++ Passenger Counter/PC.js
@@ -0,0 +1,27 @@
+// document.getElementById("count-el").innerText = 5
+
+// let count = 50
+// count = count + 50
+// console.log(count)
+
+// count = count - 75
+// console.log(count)
+
+// count = count + 45
+// console.log(count)
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+let count = 0
+
+function increment() {
+    //console.log("The button was clicked")
+    count = count + 1
+    console.log(count)
+}
+
+increment()
+
